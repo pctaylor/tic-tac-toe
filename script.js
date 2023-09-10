ccaf0393ebc43d8caa6619adacf3595418445d45
Phil Taylor <phil.c.taylor@gmail.com>
2023-09-10 09:44:44 -0400

    sober clean up

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ const GameBoard = (() => {
     let board = [...Array(3)].map(e => Array(3).fill(null));
     
     // Have a function that resets the board to its start state
-    const resetBoard = () => board.fill(null);
+    const resetBoard = () => {
+        board = [...Array(3)].map(e => Array(3).fill(null));
+    }
     return {
         board: board,
         resetBoard
@@ -13,44 +15,34 @@ const GameBoard = (() => {
 })();
 
 // Create players ----------------------------------------------
-const Player = (symbol) => {
+const Player = (name, symbol) => {
 
     // Place 'x' and 'o' on board as move
     const move = (row, column) => {
         if (GameBoard.board[row][column] === null) {
             GameBoard.board[row][column] = symbol;
+            return true;
         }
         else {
             return false
         }
     }
     return {
+        name,
         symbol,
         move
     }
 }
 
+
 // Play Game -------------------------------------------------
 const Game = (() => {
 
     // Create players
-    const player1 = Player('X');
-    const player2 = Player('O');
+    const player1 = Player("player1", 'X');
+    const player2 = Player("player2", 'O');
 
     // Game Round - Control for game flow of turns
-
-    /*
-    let solicitMove = prompt('Player 1 Move. Enter row and column separated by space');
-    [row, column] = solicitMove.split(' ');
-    player1.move(row, column);
-    console.log(GameBoard.board)
-
-    solicitMove = prompt('Player 2 Move. Enter row and column separated by space');
-    [row, column] = solicitMove.split(' ');
-    player2.move(row, column);
-    console.log(GameBoard.board)
-    */
-
     const round = (player) => {
         console.log(player.name);
         let move = false; 
@@ -58,15 +50,13 @@ const Game = (() => {
             const solicitMove = prompt(`${player.name} move. Enter row and column separated by space`);
             [row, column] = solicitMove.split(' ');
             move = player.move(row, column);
+            console.log("post loop : " + move)
         };
         console.log(GameBoard.board)
     };
     round(player1);
-    round(player2);
-    
     
 })();
-
 
 
 /*
@@ -74,6 +64,8 @@ function tests() {
     console.log(GameBoard.board);
     const player1 = Player('X');
     player1.move(1, 2);
+    console.log(GameBoard.board);
+    GameBoard.resetBoard;
     console.log(GameBoard.board);
 };
 tests();
@@ -93,3 +85,17 @@ tests();
 // Render board
 
 // Reset game with button
+
+
+
+/*
+    let solicitMove = prompt('Player 1 Move. Enter row and column separated by space');
+    [row, column] = solicitMove.split(' ');
+    player1.move(row, column);
+    console.log(GameBoard.board)
+
+    solicitMove = prompt('Player 2 Move. Enter row and column separated by space');
+    [row, column] = solicitMove.split(' ');
+    player2.move(row, column);
+    console.log(GameBoard.board)
+    */
