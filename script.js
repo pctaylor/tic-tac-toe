10cbca30d99aaa24b2ff7e1d36baac3898675945
Phil Taylor <phil.c.taylor@gmail.com>
2023-09-11 09:07:08 -0400

    Completed game

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// Tuck everything you can into a module or factory function
-
 //Set up the board game ---------------------------------------
 const GameBoard = (() => {
 
@@ -57,6 +55,11 @@ const GameBoard = (() => {
         return null;
     };
 
+    // Checking for draw
+    const checkDraw = () => {
+        return board.flat().every(cell => cell !== null);
+    };
+
     // return the board itself, reset, and win check
     return {
         get board() {
@@ -64,7 +67,8 @@ const GameBoard = (() => {
         },
         setBoard,
         checkWin,
-        resetBoard
+        resetBoard,
+        checkDraw
     };
 })();  
 
@@ -89,43 +93,51 @@ const Player = (name, symbol) => {
 }
 
 
+// Your existing GameBoard and Player code remains unchanged
+
+// Your existing GameBoard and Player code remains unchanged
+
 // Play Game -------------------------------------------------
 const Game = (() => {
-
     // Create players
     const player1 = Player("Player 1", 'X');
     const player2 = Player("Player 2", 'O');
+    let currentPlayer = player1; // Start with player 1
 
-    // Game Round - Control for game flow of turns
-    const round = (player) => {
-        console.log(player.name);
+    const gameStatus = document.querySelector('.gameStatus');
 
-        // Player move, checking for whether the move is viable
-        let move = false; 
-        while (move === false) {
-            const solicitMove = prompt(`${player.name} move. Enter row and column separated by space`);
-            [row, column] = solicitMove.split(' ');
-            move = player.move(row, column);
-        };
-        console.table(GameBoard.board);
+    // Function to handle a player's move
+    const handleMove = (row, col, cell) => {
+        if (currentPlayer.move(row, col)) {
+            cell.textContent = currentPlayer.symbol;
+            const winner = GameBoard.checkWin();
+            const isDraw = GameBoard.checkDraw();
+            if (winner) {
+                gameStatus.textContent = `${currentPlayer.name} wins!`;
+            } else if (isDraw) { // Handle draw condition
+                gameStatus.textContent = "It's a draw!";
+            } else {
+                gameStatus.textContent = `${(currentPlayer === player1) ? player2.name : player1.name}'s turn`;
+            }
+            // Switch to the other player for the next turn
+            currentPlayer = (currentPlayer === player1) ? player2 : player1;
+        }
     };
 
-    const match = (player1, player2) => {
-        const players = [player1, player2];
-        let turn = 0;
-        let winner = null; // Declare winner variable here
-        while (winner === null) {
-            const currentPlayer = players[turn % 2];
-            round(currentPlayer);
-            let statusCheck = GameBoard.checkWin();
-            console.log("Status Check: " + statusCheck);
-            if (statusCheck === currentPlayer.symbol) {
-                winner = currentPlayer.symbol;
-                console.log(`${currentPlayer.name} wins!`);
-                return currentPlayer.name;
-            }
-            turn++; // Increment the turn counter to switch players
-        }
-    };    
-    match(player1, player2);
+    // Add event listeners for cells
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach((cell, index) => {
+        const row = Math.floor(index / 3);
+        const col = index % 3;
+        cell.addEventListener('click', () => handleMove(row, col, cell));
+    });
+
+    // Add event listener for reset button
+    const resetButton = document.getElementById('reset');
+    resetButton.addEventListener('click', () => {
+        GameBoard.resetBoard();
+        cells.forEach(cell => cell.textContent = ''); // Clear the cell's text content
+        currentPlayer = player1; // Reset to player 1
+        gameStatus.textContent = 'Begin Game'; // Reset game status
+    });
 })();
